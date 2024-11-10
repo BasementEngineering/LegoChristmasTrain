77d6c9126fc25f725002a29f6b700713fa6c71a8
TransitSenseDev <j.kettler@transitsense.de>
2024-11-10 22:52:31 +0100

    Made the train motor control work, broke the display update in the process

--- ExperimentalCode/Train_PID/Train_PID.ino
+++ ExperimentalCode/Train_PID/Train_PID.ino
@@ -1,10 +1,15 @@
 #include "Speedometer.h"
 #include "SpeedMonitor.h"
+#include "TrainMotor.h"
 
+
+#define MOTOR_PIN 12
 #define ANALOG_SENSOR_PIN 32
+#define SPEED_POTI_PIN 34
 
 SpeedMonitor speedMonitor;
 Speedometer speedometer(ANALOG_SENSOR_PIN);
+TrainMotor motor(MOTOR_PIN);
 
 hw_timer_t *Timer0_Cfg = NULL;
 int counter = 0;
@@ -15,12 +20,24 @@ void IRAM_ATTR Timer0_ISR()
 }
 
 void setupTimerInterrupt(){
-  //IPC0bits.T1IP = 0b001;
-  
   Timer0_Cfg = timerBegin(1000000);
   timerAttachInterrupt(Timer0_Cfg, &Timer0_ISR);
   timerAlarm(Timer0_Cfg, speedometer.getInterval() * 1000, true, 0);
-  //timerAlarmEnable(Timer0_Cfg);
+}
+
+unsigned long lastMotorSpeedUpdate = 0;
+
+void updateMotor(){
+  if( (millis() - lastMotorSpeedUpdate) > 100){
+    int speed = map(analogRead(SPEED_POTI_PIN), 0, 4095, -100, 100);
+  if(speed < 10 && speed > -10){
+    speed = 0;
+  }
+  Serial.println(speed);
+  motor.setSpeed(speed);
+  lastMotorSpeedUpdate = millis();
+  }
+  
 }
 
 void setup() {
@@ -34,13 +51,18 @@ void setup() {
   delay(1000);
   Serial.println("Setting up encoder");
   speedometer.setupEncoder();
+  Serial.println("Setting up motor");
+  motor.begin();
   Serial.println("Activating HW Timer");
   setupTimerInterrupt();
   Serial.println("Ready to go");
+  pinMode(SPEED_POTI_PIN, INPUT);
 }
 
 void loop() {
   speedometer.update();
   speedMonitor.setSpeed(speedometer.getSpeed());
   speedMonitor.setDistance(speedometer.getDistance());
+  //speedMonitor.update();
+  updateMotor();
 }
